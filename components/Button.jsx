91160d8cfaa8367a96ac4adc6547f18a9effe12f
Artur Matysiak <inz.matys@gmail.com>
2024-05-24 19:09:49 +0200

    first commit

---
+++ components/Button.jsx
@@ -0,0 +1,6 @@
+export const Button = ({ add, del, done }) => {
+  const buttonAdd = <button className="btn-add">Click Me</button>;
+  const buttonDel = <button className="btn-del">Click Me</button>;
+  const buttonDone = <button className="btn-done">Click Me</button>;
+  return <>{}</>;
+};

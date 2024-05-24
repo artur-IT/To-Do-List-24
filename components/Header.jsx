d118149d3130465d04e73d305092bc61b4d06f0d
Artur Matysiak <inz.matys@gmail.com>
2024-05-24 20:42:39 +0200

    add Tailwind

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <section>
+      <div>
+        <h1 className="text-3xl font-bold underline">Do zrobienia</h1>
+        <p>? zadania</p>
+      </div>
+      <div>
+        <button>Plus</button>
+      </div>
+    </section>
+  );
+};
---
+++ components/NewTask.jsx
@@ -0,0 +1,10 @@
+export const NewTask = () => {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Enter Task" />
+        <button>Add Task</button>
+      </div>
+    </>
+  );
+};

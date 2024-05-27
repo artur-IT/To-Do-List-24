f6e59d9a388632ae0ab65aff3a515097ee28a899
Artur Matysiak <inz.matys@gmail.com>
2024-05-27 20:35:53 +0200

    change findAndRemoveTask function

--- components/NewTask.jsx
+++ components/NewTask.jsx
@@ -2,27 +2,25 @@ import { Button } from "./Button";
 import { Task } from "../components/Task";
 import { useRef } from "react";
 
-export const NewTask = ({ setDisplayTasks, showTasks, onAdd }) => {
+// eslint-disable-next-line react/prop-types
+export const NewTask = ({ displayTasks, setDisplayTasks, randomKey, showTasks: tableOfTasksComponents, onAdd, findTask }) => {
   const newTaskName = useRef(null);
+
   const addNewTask = () => {
-    // console.log(newTaskName.current.value);
-    if (newTaskName.current.value !== "") {
-      setDisplayTasks((prev) =>
-        prev.push(
-          // newTaskName.current.value
-          <Task taskName={newTaskName.current.value} />
-        )
-      );
-      showTasks();
-      newTaskName.current.value = "";
+    let name = newTaskName.current.value;
+    if (name !== "") {
+      // eslint-disable-next-line react/prop-types
+      setDisplayTasks((prev) => prev.push(<Task taskName={name} key={randomKey} findTask={findTask} />));
+      name = "";
     }
+    console.log(displayTasks);
   };
 
   return (
     <>
       <div className="new_task flex flex-row flex-shrink-0 justify-between items-center my-4">
         <input className="bg-slate-300 w-9/12 px-4 py-2" ref={newTaskName} type="text" placeholder="Enter Task" />
-        <Button name="add" onAdd={onAdd} showTasks={showTasks} addNewTask={addNewTask} />
+        <Button name="add" onAdd={onAdd} tableOfTasksComponents={tableOfTasksComponents} addNewTask={addNewTask} />
       </div>
     </>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,35 @@ import { Task } from "../components/Task";
 
 function App() {
   const allTasks = ["Zadanie 1", "Zadanie 2", "Zadanie 3", "Zadanie 4", "Zadanie 5"];
+  let tasksComponents = [];
   const getRandomKey = () => Math.floor(Math.random() * 10000) + 1;
 
   const findAndRemoveTask = (task) => {
-    if (allTasks.includes(task)) {
-      const index = allTasks.indexOf(task);
-      allTasks.splice(index, 1);
-      setDisplayTasks(showTasks);
-    }
+    setDisplayTasks((prev) => prev.filter((el) => el.props.taskName !== task));
   };
-  const showTasks = () => allTasks.map((task) => <Task taskName={task} key={getRandomKey()} findTask={findAndRemoveTask} />);
+  const tableOfTasksComponents = () =>
+    (tasksComponents = allTasks.map((task) => <Task taskName={task} key={getRandomKey()} findTask={findAndRemoveTask} />));
+  tableOfTasksComponents();
 
   const [showAddTask, setShowAddTask] = useState(false);
-  const [displayTasks, setDisplayTasks] = useState(showTasks());
+  const [displayTasks, setDisplayTasks] = useState(tasksComponents);
 
   return (
     <>
       <div className="w-[500px] bg-white mx-auto p-4 rounded-xl my-5">
         <Header onAdd={() => setShowAddTask(!showAddTask)} count={displayTasks.length} />
-        {showAddTask && <NewTask setDisplayTasks={setDisplayTasks} showTasks={showTasks} onAdd={() => setShowAddTask(!showAddTask)} />}
+        {showAddTask && (
+          <NewTask
+            displayTasks={displayTasks}
+            setDisplayTasks={setDisplayTasks}
+            randomKey={getRandomKey()}
+            tableOfTasksComponents={tableOfTasksComponents}
+            onAdd={() => setShowAddTask(!showAddTask)}
+            findTask={findAndRemoveTask}
+          />
+        )}
         {displayTasks}
-        {console.log(displayTasks)}
+        {/* {console.log(tasksComponents)} */}
       </div>
     </>
   );
